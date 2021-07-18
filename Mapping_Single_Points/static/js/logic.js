39bb508c54ed78b5253d71f9dc04851597874dbb
Dpak-Ch <dpak.ch@hotmail.com>
2021-07-17 21:08:05 -0500

    Mapping Single Points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -11,9 +11,10 @@ let map = L.map("mapid", {
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: 'mapbox/dark-v10',
     accessToken: API_KEY
 });
 
@@ -21,8 +22,8 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
+L.circleMarker([34.0522, -118.2437], {
+  radius: 100,
   color: 'black',
-  fillColor: 'red',
-  radius: 300
+  fillColor: '#ffffa1'
 }).addTo(map);
